f24f7ba310683b4b195fcc5b1236083f3b387b42
laljidp <yuvalprajapati572@gmail.com>
2023-12-18 12:20:25 +0530

    fixed: deployment issue with next auth.

---
+++ app/api/auth/[...nextauth]/authOptions.ts
@@ -0,0 +1,41 @@
+import GoogleProvider from "next-auth/providers/google";
+import {
+  UserPayloadT,
+  createUserIfNotExists,
+} from "@/services/db/functions/users.functions";
+import { AuthOptions } from "next-auth";
+import { config } from "@/constants";
+
+export const authOptions: AuthOptions = {
+  // Secret for Next-auth, without this JWT encryption/decryption won't work
+  secret: config.jwtSecretKey,
+  // Configure one or more authentication providers
+  providers: [
+    GoogleProvider({
+      clientId: config.gClientId || "",
+      clientSecret: config.gClientSecret || "",
+    }),
+    // ...add more providers here
+  ],
+  callbacks: {
+    signIn: async ({ user, account }) => {
+      const payload: UserPayloadT = {
+        email: user.email || "",
+        name: user.name || "",
+        picture: user.image || "",
+        provider: account?.provider || "",
+        providerId: account?.providerAccountId || "",
+        role: "user",
+        type: "oauth",
+      };
+      const result = await createUserIfNotExists(payload);
+      if (result === "exists") {
+        console.log("User already existed!");
+      }
+      if (result === "created") {
+        console.log("New user created");
+      }
+      return true;
+    },
+  },
+};
--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,43 +1,5 @@
-import { config } from "@/constants";
-import {
-  UserPayloadT,
-  createUserIfNotExists,
-} from "@/services/db/functions/users.functions";
-import NextAuth, { AuthOptions } from "next-auth";
-import GoogleProvider from "next-auth/providers/google";
-export const authOptions: AuthOptions = {
-  // Secret for Next-auth, without this JWT encryption/decryption won't work
-  secret: config.jwtSecretKey,
-  // Configure one or more authentication providers
-  providers: [
-    GoogleProvider({
-      clientId: config.gClientId || "",
-      clientSecret: config.gClientSecret || "",
-    }),
-    // ...add more providers here
-  ],
-  callbacks: {
-    signIn: async ({ user, account }) => {
-      const payload: UserPayloadT = {
-        email: user.email || "",
-        name: user.name || "",
-        picture: user.image || "",
-        provider: account?.provider || "",
-        providerId: account?.providerAccountId || "",
-        role: "user",
-        type: "oauth",
-      };
-      const result = await createUserIfNotExists(payload);
-      if (result === "exists") {
-        console.log("User already existed!");
-      }
-      if (result === "created") {
-        console.log("New user created");
-      }
-      return true;
-    },
-  },
-};
+import NextAuth from "next-auth";
+import { authOptions } from "./authOptions";
 
 const handler = NextAuth(authOptions);
 export { handler as GET, handler as POST };
