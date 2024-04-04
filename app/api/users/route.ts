a7c3519f12e4d269806765af3e86d064994e522d
laljidp <yuvalprajapati572@gmail.com>
2024-04-04 21:39:53 +0530

    fix: build error.

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -9,13 +9,10 @@ import {
 } from "@/utils/nextResponse.util";
 import { NextRequest } from "next/server";
 
-export async function GET(
-  req: NextRequest,
-  { params }: { params: FetchUsersParamsT }
-) {
+export async function GET(req: NextRequest) {
+  const params = req.nextUrl.searchParams;
   try {
     await connectDB();
-    const params = req.nextUrl.searchParams;
     const batch = (params.get("batch") || 20) as number;
     const page = (params.get("page") || 0) as number;
     const searchText = params.get("searchText") as string;
